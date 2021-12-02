ef84b890ff579ed949df3129218afa75d528c62d
Pat Kelly <PatK80@gmail.com>
2021-12-02 17:50:22 -0500

    Header section complete

--- dist/custom.18d3a4a7.js
+++ dist/custom.18d3a4a7.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"_css_loader":"../../../AppData/Roaming/nvm/v10.15.1/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../../../AppData/Roaming/nvm/v10.15.1/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./..\\assets\\usa-flag-header.jpg":[["usa-flag-header.e54d9482.jpg","../assets/usa-flag-header.jpg"],"../assets/usa-flag-header.jpg"],"_css_loader":"../../../AppData/Roaming/nvm/v10.15.1/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../../../AppData/Roaming/nvm/v10.15.1/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
